69a020e61a3df2db2ffe84a31741fda3f5b9ab2a
David Borg <david@greenjade.com>
2023-03-22 21:10:03 +0100

    making changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    "reportDir": "cypress/reports",
+    "charts": true,
+    "reportPageTitle": "Casumo-Cypress",
+    "reportFilename": "Devowelizer_Report",
+    "embeddedScreenshots": true,
+    "inlineAssets": true,
+    "saveAllAttempts": true,
+  },
+
+  video: false,
 
   e2e: {
     component: {
@@ -13,6 +26,7 @@ module.exports = defineConfig({
     baseUrl: 'http://localhost:8080',
     
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       const webpackPreprocessor = require('@cypress/webpack-preprocessor')
       const options = {
         // send in the options from your webpack.config.js, so it works the same
